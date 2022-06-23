8c5ffeb253773835d977a662926bd9ec012888f6
Lad4ibk <ibk4lad@gmail.com>
2022-06-23 07:17:48 +0100

    adding files for first java task

---
+++ aboutme.js
@@ -0,0 +1,10 @@
+let myName = "Ibukun Ladeji"
+
+let myHeight = "5.5 feets"
+
+let myCountry = "Nigeria"
+
+
+document.write('Name:' + ' ' + ' ' + myName + '<br>');
+document.write('Height:' + ' ' + ' ' + myHeight + '<br>');
+document.write('Country:' + ' ' + ' ' + myCountry + '<br>');
